82f20ee110b2e020f89a697e9e470c1e2bba6d2a
halida03 <sakinadzeenbekova@gmail.com>
2023-12-14 22:27:05 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import "./App.css";
+import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import MovieDetail from "./pages/movieDetail";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:movieId" element={<MovieDetail />} />
       </Routes>
+      <Footer/>
     </>
   );
 };
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import Footer from "../../components/Footer";
-import Header from "../../components/Header";
 import '../home/home.scss'
 import MovieListing from "../../components/MovieListing/Index";
 
 const Home = () => {
   return <div className="home">
-    <Header />
     <MovieListing />
-    <Footer />
   </div>;
 };
 
